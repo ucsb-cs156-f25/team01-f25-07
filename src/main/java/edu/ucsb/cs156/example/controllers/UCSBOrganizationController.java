b644cb2094e5d447ca4131ce55cf96beca4071a6
Yuchao Zheng <yuchaozheng@ucsb.edu>
2025-10-24 13:50:30 -0700

    update

--- src/main/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationController.java
@@ -67,7 +67,7 @@ public class UCSBOrganizationController extends ApiController {
   /**
    * This method returns a single organization.
    *
-   * @param code code of the organization
+   * @param orgCode code of the organization
    * @return a single organization
    */
   @Operation(summary = "Get a single UCSBOrganization by orgCode")

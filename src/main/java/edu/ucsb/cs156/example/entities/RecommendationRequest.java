73311d179aa4863cdf33044d071b41fb5b76f211
Sicheng <sxu944@ucsb.edu>
2025-10-23 20:32:31 -0700

    Fix: add dateRequested and dateNeeded to RecommendationRequest

--- src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequest.java
+++ src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequest.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,5 +24,7 @@ public class RecommendationRequest {
   private String requesterEmail;
   private String professorEmail;
   private String explanation;
+  private LocalDateTime dateRequested;
+  private LocalDateTime dateNeeded;
   private boolean done;
 }
